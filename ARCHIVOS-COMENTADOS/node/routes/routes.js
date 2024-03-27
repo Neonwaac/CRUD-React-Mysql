f6c8cd407b54c2fb5792fb5e411513451015b647
Neonwaac12 <124847308+Neonwaac@users.noreply.github.com>
2024-03-26 23:47:02 -0500

    Update routes.js

--- ARCHIVOS-COMENTADOS/node/routes/routes.js
+++ ARCHIVOS-COMENTADOS/node/routes/routes.js
@@ -1,11 +1,18 @@
+// Importar el framework Express
 import express from 'express'
+
+// Importar funciones del controlador BlogController
 import { createBlog, deleteBlog, getAllBlogs, getBlog, updateBlog } from '../controllers/BlogController.js'
 
+// Crear un nuevo enrutador de Express
 const router = express.Router()
-router.get('/', getAllBlogs)
-router.get('/:id',getBlog)
-router.post('/', createBlog)
-router.put('/:id', updateBlog)
-router.delete('/:id', deleteBlog)
 
+// Definir las rutas y sus controladores correspondientes
+router.get('/', getAllBlogs)      // Ruta para obtener todos los blogs
+router.get('/:id', getBlog)        // Ruta para obtener un blog por su ID
+router.post('/', createBlog)       // Ruta para crear un nuevo blog
+router.put('/:id', updateBlog)     // Ruta para actualizar un blog existente por su ID
+router.delete('/:id', deleteBlog)  // Ruta para eliminar un blog por su ID
+
+// Exportar el enrutador para su uso en otras partes de la aplicación
 export default router
